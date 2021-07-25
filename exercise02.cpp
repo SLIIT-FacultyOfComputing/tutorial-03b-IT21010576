69b302c26ef2085f14a7033a379398cd55640636
it21010576 <87602590+IT21010576@users.noreply.github.com>
2021-07-25 10:51:23 +0000

    exercise 02 completed

--- exercise02.cpp
+++ exercise02.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int volume(int height, int width, int length);
 
-int main() {
+int main() 
+{
     int box1Height, box1Width, box1Length;
     int box2Height, box2Width, box2Length;
     int totalVolume, totalSurface;
@@ -29,5 +30,8 @@ int main() {
     
     return 0;
 }
+int volume(int height, int width, int length)
+{
+  return height * width * length;
+}
 
-// Implement the Volume() function here
